bfd471523c76c4a7d45dd512dc7792e703a4b398
moeshin <764686875@qq.com>
2020-02-22 05:51:13 +0800

    将相对路径下的 md 后缀改为 html

--- gitbook-plugin-custom/index.js
+++ gitbook-plugin-custom/index.js
@@ -7,20 +7,36 @@ module.exports = {
     hooks: {
         "page": function (page) {
             const $ = cheerio.load(page.content);
-            $('a').each(function(i, elem) {
-                const a = $(elem);
-                const src = a.attr("href");
-                if (src !== a.html()) {
-                    return;
+            const arr = $('a');
+            const length = arr.length;
+            for (let i = 0; i < length; i++) {
+                const a = $(arr[i]);
+                const src = a.attr('href');
+                if (typeof src !== 'string') {
+                    continue;
                 }
-                const matches = src.match(/^https?:\/\/(?:www|m).bilibili.com\/video\/av(\d+).*(?:[?&]p=(\d+))(?:&|$)/);
+                if (src === a.html()) {
+                    // 生成 bilibili 播放器
+                    const matches = src.match(/^https?:\/\/(?:www|m).bilibili.com\/video\/av(\d+).*(?:[?&]p=(\d+))(?:&|$)/);
+                    if (!matches) {
+                        throw new Error('Format Error：' + src);
+                    }
+                    const av = matches[1];
+                    const page = matches[2] || 1;
+                    a.replaceWith(`<div class="bilibili"><iframe src="https://player.bilibili.com/player.html?aid=${av}&page=${page}" allowfullscreen></iframe></div>`);
+                    continue;
+                }
+                // 将相对路径下的 md 后缀改为 html
+                const matches = src.match(/^([^:]+?)\.md([#?].*)?$/);
                 if (!matches) {
-                    throw new Error('Format Error：' + src);
+                    continue;
                 }
-                const av = matches[1];
-                const page = matches[2] || 1;
-                a.replaceWith(`<div class="bilibili"><iframe src="https://player.bilibili.com/player.html?aid=${av}&page=${page}" allowfullscreen></iframe></div>`);
-            });
+                let href = matches[1] + '.html';
+                if (matches[2]) {
+                    href += matches[2];
+                }
+                a.attr('href', href);
+            }
             page.content = $.html();
             return page;
         }
